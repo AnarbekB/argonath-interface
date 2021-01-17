6710b491b3619ed5fa3b19256e5c0afaa31e1534
anarbek <balmukanovab@gmail.com>
2021-01-17 20:13:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+thrift interface for call argonath functions
+
+generate code:
+thrift -r -out src/main/java/ru/balmukanov/productmarketinterface/thrift/ --gen java src/main/resources/thrift/service.thrift
---
+++ src/main/resources/thrift/service.thrift
@@ -0,0 +1,15 @@
+namespace java ru.balmukanov.argonathinterface.thrift
+
+exception WrongPhoneOrPasswordException {
+    1: i32 code,
+    2: string description
+}
+
+struct Session {
+    1: i64 id,
+    2: string jwt
+}
+
+service ProductMarketThriftService {
+    Session login(1:string phone, 2:string password, 3:string channel) throws (2:WrongPhoneOrPasswordException e),
+}
